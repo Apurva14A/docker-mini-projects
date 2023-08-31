67a797866b6ddcbb5306cf9efc54eb9686237dfa
Ashutosh Apurva <apooorva01@gmail.com>
2023-08-31 01:31:32 +0000

    Added docker compose file

--- custom-app-image/diff_cmd_&_entrypoint/args.sh
+++ custom-app-image/diff_cmd_&_entrypoint/args.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
-# Taking input to pass argument 
+# Getting Image Versions details
 
-ARGS=read name
+IMAGE=$(docker images -f "reference:nodejs-apps:v1.0.0-alpine-todo")
 
+# show image tags in DockerHub
+echo
+echo crane ls $IMAGE
+crane ls $IMAGE
+
+TAG=v1.0.0-alpine-todo
+
+# Getting image size 
+
+echo
+echo docker inspect $IMAGE:$TAG
+docker inspect $IMAGE:$TAG | jq -r '.' | tee docker-inspect-$IMAGE-$TAG.json
+
+echo Getting size for $IMAGE:$TAG
+size=$(docker inspect $IMAGE:$TAG | jq '.[].Size')
+echo size=$size
+size_in_mb=$((size / (1024*1024)))
+echo size_in_mb=$size_in_mb MB
